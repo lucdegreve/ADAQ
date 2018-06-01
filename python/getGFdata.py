da6630729388228efa0f985bb831c5d775ebf8ca
Alexandre <alexandre.mertens1@gmail.com>
2018-06-01 16:00:55 +0200

    Trying to combine Download and csv conversion -> currently fail

--- python/getGFdata.py
+++ python/getGFdata.py
@@ -1,23 +1,67 @@
-from robobrowser import RoboBrowser
+# This code :
+#   -connects to the greenfeed website, download the summary data
+#   -save a csv file with the interesting data.
+# Authors : Luc Degreve, Alexandre Mertens
 
-base_url = "https://greenfeed.c-lockinc.com/GreenFeed/home.php"
-base_url_xls = "https://greenfeed.c-lockinc.com/GreenFeed/downloaddata/downloaddailyfiles.php?file=GreenFeed_Summarized_Data_Belgium_90_91.xlsm"
+from robobrowser import RoboBrowser    # website browsing
+import xlrd                            # Excel file reading
+from xlwt import Workbook              # Excel file writing
 
-# definition of the browser
-browser = RoboBrowser()
+downloadData = True
+convertToCsv = True
 
-# Access the GreenFeed websiteand fill the login form
-browser.open(base_url)
-form = browser.get_form(action="checklogon.php")
-form["username"] = '' # to be completed!
-form["password"] = '' # to be completed!
-browser.session.headers['Referer'] = base_url
-browser.submit_form(form, submit='Login')
 
-# create the request to access the xls file
-request = browser.session.get(base_url_xls, stream=True)
+def getGFSummary(outFile):
 
-# save the xls file
-with open("test.xls", "wb") as xls_file:
-    xls_file.write(request.content)
+    base_url = "https://greenfeed.c-lockinc.com/GreenFeed/home.php"
+    base_url_xls = "https://greenfeed.c-lockinc.com/GreenFeed/downloaddata/downloaddailyfiles.php?file=GreenFeed_Summarized_Data_Belgium_90_91.xlsm"
 
+    # definition of the browser
+    browser = RoboBrowser()
+
+    # Access the GreenFeed websiteand fill the login form
+    browser.open(base_url)
+    form = browser.get_form(action="checklogon.php")
+    form["username"] = ''  # to be completed!
+    form["password"] = ''  # to be completed!
+    browser.session.headers['Referer'] = base_url
+    browser.submit_form(form, submit='Login')
+
+    # create the request to access the xls file
+    request = browser.session.get(base_url_xls, stream=True)
+
+    with open(outFile, "wb") as test:
+        test.write(request.content)
+
+    return()
+
+
+def excelToCsv(xls_file):
+
+    # xls file opening and sheet selection
+    document = xlrd.open_workbook(xls_file)
+    data = document.sheet_by_index(0)
+
+    # Getting the number of lines and columns
+    nbrow = data.nrows
+    nbcol = data.ncols
+
+    # Creation of the new file
+    book = Workbook()
+    sheet1 = book.add_sheet('view_data')
+
+    for k in range(0, nbcol):
+        for i in range(0, nbrow-1):
+            sheet1.write(i, k, data.cell_value(rowx=(i+1), colx=k))
+
+    return(book)
+
+
+# get GF summary, transform to csv and save in the csv
+xls_file = "../csv/GreenFeed/GF_Summary.xls"
+if downloadData:
+    getGFSummary(xls_file)
+
+if convertToCsv:
+    csv_file = excelToCsv(xls_file)
+    csv_file.save("../csv/GreenFeed/GF_Summary.csv")
