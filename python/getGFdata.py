0727c64fa7f2dcb6825f028a53c422858d709e05
Unknown <lucdegreve@hotmail.fr>
2018-06-15 15:40:02 +0200

    Tableau Animaux

--- python/getGFdata.py
+++ python/getGFdata.py
@@ -1,3 +1,4 @@
+
 # This code :
 #   -connects to the greenfeed website, download the summary data
 #   -save a csv file with the interesting data.
@@ -8,6 +9,7 @@ import xlrd                             # Excel file reading
 from xlwt import Workbook               # Excel file writing
 import pandas as pd                     # Csv file writing
 from datetime import date               # Date
+from datetime import timedelta, datetime
 import os                               # To delete a file
 
 downloadData = True
@@ -19,7 +21,7 @@ getDateCali = True
 getDateRec = True
 getStatus = True
 colData = True
-
+tableau = True
 def GFLogin(login,password):
     base_url = "https://greenfeed.c-lockinc.com/GreenFeed/home.php"
 
@@ -66,7 +68,7 @@ def selectData(xls_file):
 
     return(book)
 
-def excelToCsv(xls_file,exitFile):
+def excelToCsv(xls_file, exitFile):
     today = str(date.today())
     df = pd.read_excel(xls_file)
     df.to_csv(exitFile + '_' + today, index=False)
@@ -115,6 +117,91 @@ def Status():
     indfeedstart = str.find(feederinfo, "<status>")
     indfeedend = str.find(feederinfo,"</status>")
     return(feederinfo[indfeedstart+8:indfeedend])
+
+def Recherche(cont_page, val,lent=None, valend=None):
+    indini = 0
+    listeind = []
+    while indini!=(-1):
+        indtableau = str.find(cont_page, val,indini+1)
+        listeind.append(indtableau)
+        indini = indtableau
+    listeind.remove(-1)
+    lenlisind = len(listeind)
+    listval = []
+    listeindend = []
+    if (lent!=None):
+        for k in range(0, lenlisind):
+            listval.append(cont_page[listeind[k]+len(val):listeind[k]+len(val)+lent])
+    else :
+        indini = 0
+        while indini != (-1):
+            indtableauend = str.find(cont_page, valend, indini + 1)
+            listeindend.append(indtableauend)
+            indini = indtableauend
+        listeindend.remove(-1)
+        for y in range(0, lenlisind):
+            listval.append(cont_page[listeind[y]+len(val):listeindend[y]])
+    return listval
+
+def getTableau():
+    temps = timedelta(days=7)
+    temps2 = timedelta(days=30)
+    arrive = datetime.today()
+    depart = arrive - temps
+    depart2 = arrive - temps2
+    departt = str(depart)
+    arrivet = str(arrive)
+    departt2 = str(depart2)
+    browser.open("https://greenfeed.c-lockinc.com/GreenFeed/tabledata/cowfeeding.php?fids=0,91&from="+departt[0:10]+"&to="+arrivet[0:10]+"&cons=0&uncons=0&param=1")
+
+    tableaubrute = str(browser.parsed)
+
+    listtag = Recherche(tableaubrute,'tag="',24)
+    listnom = Recherche(tableaubrute,'name="',8)
+    listdate = Recherche(tableaubrute, "<date>",5)
+    listdrop = Recherche(tableaubrute,"<v>",None, '</v>')
+    listdrop= list(zip(*[iter(listdrop)]*len(listdate)))
+
+
+
+    browser.open("https://greenfeed.c-lockinc.com/GreenFeed/tabledata/cowfeeding.php?fids=0,91&from="+departt2[0:10]+"&to="+arrivet[0:10]+"&cons=0&uncons=0&param=1")
+
+    tableaubrute2 = str(browser.parsed)
+    listdate2 = Recherche(tableaubrute2, "<date>", 5)
+    listdrop2 = Recherche(tableaubrute2,"<v>",None, '</v>')
+    listdrop2= list(zip(*[iter(listdrop2)]*len(listdate2)))
+    listnom2 = Recherche(tableaubrute2,'name="',8)
+    ind = []
+    for a in listnom:
+        ind.append(listnom2.index(a))
+    somind = []
+    for e in range(len(listnom2)):
+        som = 0
+        for r in range(len(listdate2)):
+            som += int(listdrop2[e][r])
+        somind.append(som)
+    bonval = []
+    for y in ind :
+        bonval.append(somind[y])
+
+
+    # excel
+    book3 = Workbook()
+    sheet1 = book3.add_sheet("Tableau")
+    sheet1.write(0,0,"Animal Name")
+    sheet1.write(0,1,"Animal Tag")
+    sheet1.write(0,len(listdate)+2,"Total 30j")
+    for k in range(1, len(listnom)+1):
+        sheet1.write(k,1,listtag[k-1])
+        sheet1.write(k,0,listnom[k-1])
+        sheet1.write(k,len(listdate)+2,bonval[k-1])
+    for i in range(2, len(listdate)+2):
+        sheet1.write(0,i,listdate[i-2])
+    for l in range(len(listdrop)):
+        for z in range(len(listdate)):
+            sheet1.write(l+1,z+2,listdrop[l][z])
+
+    return(book3)
 
 def exelColData():
     # Creation of the new file
@@ -172,3 +259,13 @@ if convertToCsv and colData:
 
 if deleteXls and colData:
     os.remove('Col_Data.xls')
+
+if tableau:
+    xls_file3 = getTableau()
+    xls_file3.save('tableau.xls')
+
+if convertToCsv and tableau:
+	excelToCsv("tableau.xls","Tableau")
+
+if deleteXls and tableau:
+    os.remove('tableau.xls')
