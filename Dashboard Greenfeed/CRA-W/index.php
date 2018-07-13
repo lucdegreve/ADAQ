b5c67b46f1d24d2bbf806f6057ff00c3d7ccbace
Unknown <lucdegreve@hotmail.fr>
2018-07-13 11:45:42 +0200

    Dashboard Greenfeed V1

---
+++ Dashboard Greenfeed/CRA-W/index.php
@@ -0,0 +1,244 @@
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Greenfeed</title>
+    <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" type="image/x-icon" href="image/favicon.ico" />
+	<meta http-equiv="refresh" content="1800" />
+  </head>
+  <body>
+    <div class="container-fluide">
+
+    <!-------->
+    <div id="content">
+        <ul id="tabs" class="nav nav-tabs onglet" data-tabs="tabs">
+            <li><a href="#red" data-toggle="tab">Greenfeed 90</a></li>
+            <li class="active"><a href="#orange" data-toggle="tab">Greenfeed 91</a></li>
+        </ul>
+        <div id="my-tab-content" class="tab-content">
+          <!----------Greenfeed 90--------->
+            <div class="tab-pane" id="red">
+              <div class="container-fluid entête">
+                <div class="row">
+                  <div class="col-md-1 bouton">
+                    <?php
+                    $nomcol='C:\Dashboard Greenfeed\Greenfeed_90\colData_90';
+                    $len_col=count(file($nomcol));
+                    if (substr(cellule($nomcol,$len_col,1),0,6)=='Online')
+                      echo'<img src="image/bouton_online.png" alt="logo online" width=75px />';
+                    elseif (cellule($nomcol,$len_col,1)=='Offline')
+                      echo'<img src="image/bouton_offline.png" alt="logo online" width=75px />';
+                    else
+                      echo'<img src="image/bouton_autre.png" alt="logo online" width=75px />';
+                    ?>
+                  </div>
+                  <div class="col-md-4 statut">
+                    <?php
+                    $len_col=count(file($nomcol));
+                    echo cellule($nomcol,$len_col,0).' :  '.cellule($nomcol,$len_col,1);
+                    ?>
+                  </div>
+                  <div class="col-md-6 colonne-centree">
+                    <img src="image/Logo_CRA-W.png" alt="logo CRA-W" />
+                  </div>
+                  <div class="col-md-1 statut">
+                    <?php
+                    $len_col=count(file($nomcol));
+
+                    $datetime1 = new DateTime(cellule($nomcol,$len_col-1,1));
+                    $datetime2 = new DateTime(date("Y-m-d"));
+                    $interval = $datetime1->diff($datetime2);
+                    $maj=(cellule($nomcol,$len_col-1,1));
+                    $c =$interval->format('%a');
+                    if($c < 1){
+                      $couleur = "#ddd";
+                    }
+                    else {
+                      $couleur = "red";
+                    }
+                    echo"<font color=".$couleur.">$maj</font>";
+                    ?>
+                  </div>
+                </div>
+              </div>
+              <div class="container">
+                <div class="row contenu">
+                  <div class="col-md-4 col">
+                    <div class="row">
+                      <div class="col-md-6 nomcol">
+                        <?php
+                        $len_col=count(file($nomcol));
+                        for ($k=1;$k<=$len_col-2;$k++){
+                          echo cellule($nomcol,$k,0)." :   <br/><br/>";
+                        }
+                         ?>
+                      </div>
+                      <div class="col-md-6 valcol">
+                        <?php
+                        $len_col=count(file($nomcol));
+                        for ($k=1;$k<=$len_col-2;$k++){
+                          $valeur=cellule($nomcol,$k,1)."<br/><br/>";
+                          $couleurv=cellule($nomcol,$k,2);
+                          echo"<font color=".$couleurv.">$valeur</font>";
+                        }
+                         ?>
+                      </div>
+
+                    </div>
+
+                  </div>
+                  <div class="col-md-4 tableau">
+
+                    <table class="table table-striped">
+                      <thead>
+                        <tr>
+                          <?php
+                          $nomtab='C:\Dashboard Greenfeed\Greenfeed_90\Tableau_90';
+                          for ($i=0;$i<=10;$i++){
+                            echo"<td>".cellule($nomtab,1,$i)."</td>";
+                          } ?>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <?php
+                        $len_colt=count(file($nomtab));
+                        for ($k=2;$k<=$len_colt;$k++){
+                          echo "<tr>";
+                          for($i=0;$i<=10;$i++){
+                            echo '<td>'.cellule($nomtab,$k,$i).'</td>';
+                          } echo '</tr>';
+                        }
+                        ?>
+                      </tbody>
+                    </table>
+
+                  </div>
+                </div>
+
+              </div>
+            </div>
+          <!----------Greenfeed 91--------->
+            <div class="tab-pane active" id="orange">
+              <div class="container-fluid entête">
+                <div class="row">
+                  <div class="col-md-1 bouton">
+                    <?php
+                    $nomcol='C:\Dashboard Greenfeed\Greenfeed_91\colData_91';
+                    $len_col=count(file($nomcol));
+                    if (substr(cellule($nomcol,$len_col,1),0,6)=='Online')
+                      echo'<img src="image/bouton_online.png" alt="logo online" width=75px />';
+                    elseif (cellule($nomcol,$len_col,1)=='Offline')
+                      echo'<img src="image/bouton_offline.png" alt="logo online" width=75px />';
+                    else
+                      echo'<img src="image/bouton_autre.png" alt="logo online" width=75px />';
+                    ?>
+                  </div>
+                  <div class="col-md-4 statut">
+                    <?php
+                    $len_col=count(file($nomcol));
+                    echo cellule($nomcol,$len_col,0).' :  '.cellule($nomcol,$len_col,1);
+                    ?>
+                  </div>
+                  <div class="col-md-6 colonne-centree">
+                    <img src="image/Logo_CRA-W.png" alt="logo CRA-W" />
+                  </div>
+                  <div class="col-md-1 statut">
+                    <?php
+                    $len_col=count(file($nomcol));
+
+                    $datetime1 = new DateTime(cellule($nomcol,$len_col-1,1));
+                    $datetime2 = new DateTime(date("Y-m-d"));
+                    $interval = $datetime1->diff($datetime2);
+                    $maj=(cellule($nomcol,$len_col-1,1));
+                    $c =$interval->format('%a');
+                    if($c < 1){
+                      $couleur = "#ddd";
+                    }
+                    else {
+                      $couleur = "red";
+                    }
+                    echo"<font color=".$couleur.">$maj</font>";
+                    ?>
+                  </div>
+                </div>
+              </div>
+              <div class="container">
+                <div class="row contenu">
+                  <div class="col-md-4 col">
+                    <div class="row">
+                      <div class="col-md-6 nomcol">
+                        <?php
+                        $len_col=count(file($nomcol));
+                        for ($k=1;$k<=$len_col-2;$k++){
+                          echo cellule($nomcol,$k,0)." :   <br/><br/>";
+                        }
+                         ?>
+                      </div>
+                      <div class="col-md-6 valcol">
+                        <?php
+                        $len_col=count(file($nomcol));
+                        for ($k=1;$k<=$len_col-2;$k++){
+                          $valeur=cellule($nomcol,$k,1)."<br/><br/>";
+                          $couleurv=cellule($nomcol,$k,2);
+                          echo"<font color=".$couleurv.">$valeur</font>";
+                        }
+                         ?>
+                      </div>
+
+                    </div>
+
+                  </div>
+                  <div class="col-md-4 tableau">
+
+                    <table class="table table-striped">
+                      <thead>
+                        <tr>
+                          <?php
+                          $nomtab='C:\Dashboard Greenfeed\Greenfeed_91\Tableau_91';
+                          for ($i=0;$i<=10;$i++){
+                            echo"<td>".cellule($nomtab,1,$i)."</td>";
+                          } ?>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <?php
+                        $len_colt=count(file($nomtab));
+                        for ($k=2;$k<=$len_colt;$k++){
+                          echo "<tr>";
+                          for($i=0;$i<=10;$i++){
+                            echo '<td>'.cellule($nomtab,$k,$i).'</td>';
+                          } echo '</tr>';
+                        }
+                        ?>
+                      </tbody>
+                    </table>
+
+                  </div>
+                </div>
+
+              </div>
+              <?php
+              //lecture d'un fichier cellule par cellule
+              //ligne 1-10
+              //colonne 0-9
+              function cellule($csv, $lig, $col){
+              	$fic=fopen($csv,'r');
+              	$len=count(file($csv));
+              	for ($i=1;$i<=$lig;$i++){
+              		$ligne=fgetcsv($fic,1024,',');
+              	}
+              	fclose($fic) ;
+              	return $ligne[$col];
+              }
+              ?>
+            </div>
+        </div>
+    </div>
+  </body>
+</html>
---
+++ Dashboard Greenfeed/README.txt
@@ -0,0 +1,39 @@
+Contenu du dossier:
+	- README.txt
+	- DashboardGF.bat
+	- getGFdatabyH.py
+	- getGFdatabyD.py
+	- CRA-W
+		>css
+			>bootstrap.min.css
+			>style.css
+		>image
+			>bouton_online.png
+			>bouton_offline.png
+			>bouton_autre.png
+			>Logo_CRA-W.png
+			>favicon.ico
+		>index.php
+	- Summary
+	- Raw_Data
+	- Greenfeed_90
+	- Greenfeed_91
+	
+Placer ce dossier à la racine de votre disque C:
+
+Télécharger php avec ce lien : http://php.net/downloads.php
+Selectionner le lien windows downloads de la dernière version stable.
+Puis télécharger le Zip Non Thread qui correspond à votre version 64 bits ou 32 bits.
+Créer un dossier nommé "phpresource" à la racine de votre disque C:.
+Extrair le Zip à l'intérieur sans créer de dossier intermédiaire.
+Placer également le document CRA-W dans le dossier phpresource.
+
+Télécharger python avec le lien : https://www.python.org/downloads/
+Une fois l'installation fini editer le script DashboardGF.bat qui se situe dans le dossier "Dashboard Greenfeed".
+et modifier le chemin jusqu'a python.exe avec le chemin de votre ordinateur.
+
+Exemple C:\Users\l.degreve\AppData\Local\Programs\Python\Python37-32
+ 
+Faîtes de même avec le chemin jusqu'a votre navigateur.
+Placer le fichier getGFdatabyD.py et getGFdatabyH.py dans le dossier qui contient python. 
+Voila vous pouvez lancer getGFdata.bat 
